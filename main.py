2b4f84a4a2f83d7412896c7f2e5aa2b1482a69de
ryuesemiel <jihiyo7@hanyang.ac.kr>
2024-12-31 03:42:51 +0900

    elf

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('yeah lostark players')
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
